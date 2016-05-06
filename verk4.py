0a17da72bdac22a17e6240dc5c8085a042d0f04f
Eysteinn Gunnlaugsson <eysteinn13@ru.is>
2016-05-06 14:36:02 +0000

    flot

--- verk4.py
+++ verk4.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-# from os import walk as w
 import os
 import shutil
 # from os.path import join as j
-def sort(directory, show, search_string):
+def sort(directory, show):
     if not os.path.exists(directory):
         os.makedirs(directory) # add directory
         shutil.rmtree(directory) # remove directory
 
-sort('shows', 'bla', 'bla')
+sort('shows', 'bla')
