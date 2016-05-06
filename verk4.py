4ab65cf6b6f7d9ee3d980c78c0f0e061985709be
Eysteinn Gunnlaugsson <eysteinn13@ru.is>
2016-05-06 14:43:20 +0000

    flot

--- verk4.py
+++ verk4.py
@@ -7,4 +7,15 @@ def sort(directory, show):
         os.makedirs(directory) # add directory
         shutil.rmtree(directory) # remove directory
 
+# returns a list of a few generated search strings
+# E.g input = The Big Bang Theory
+#       retunrs thebigbangtheory
+#               tbbt
+#               the.big.bang.theory
+#               the-big-bang-theory            
+def make_search_strings(str):
+
+
+
+
 sort('shows', 'bla')
