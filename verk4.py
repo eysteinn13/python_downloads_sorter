9d1eebb72e258fc5e7c40db8f021af2fd1c9ccbf
vala14 <vala14@ru.is>
2016-05-06 14:57:58 +0000

    flot

--- verk4.py
+++ verk4.py
@@ -5,17 +5,29 @@ import shutil
 def sort(directory, show):
     if not os.path.exists(directory):
         os.makedirs(directory) # add directory
+        make_search_strings(show)
         shutil.rmtree(directory) # remove directory
 
 # returns a list of a few generated search strings
 # E.g input = The Big Bang Theory
-#       retunrs thebigbangtheory
+#       returns thebigbangtheory
 #               tbbt
 #               the.big.bang.theory
-#               the-big-bang-theory            
-def make_search_strings(str):
+#               the-big-bang-theory
+def make_search_strings(inp):
+    inp = inp.lower()
+    l = []
+    t = inp.replace(' ', '.')
+    l.append(t)
+    t = inp.replace(' ', '-')
+    l.append(t)
+    t = inp.replace(' ', '')
+    l.append(t)
+    t = inp.split(' ')
+    t = ''.join([i[0] for i in t])
+    l.append(t)
 
+    print l
+    return l
 
-
-
-sort('shows', 'bla')
+sort('shows', 'The Big Bang Theory')
